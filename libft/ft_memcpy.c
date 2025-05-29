3d4d8a7e2513de412345690dc2a9be9226171799
Yaşam Ensar Demirkıran <yademirk@student.42istanbul.com.tr>
2025-05-29 12:04:59 +0300

    Add the correct 42 headers

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 11:59:15 by yademirk          #+#    #+#             */
+/*   Updated: 2025/05/29 12:00:43 by yademirk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	if (dest == (void *)0 && src == (void *)0)
 		return (dest);
-
 	while (n-- > 0)
 		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
 	return (dest);
