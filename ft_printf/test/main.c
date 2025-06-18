ae841fb99013412dac9465901be7e0d9dce16d6b
YeDemirkiran <yedemirkiran27@gmail.com>
2025-06-18 17:13:09 +0300

    Add new tests

--- ft_printf/test/main.c
+++ ft_printf/test/main.c
@@ -2,40 +2,60 @@
 
 int	main(void)
 {
+	int	res;
 	printf("ft_printf() Test Programı (by yademirk)");
 
 	printf("\n\nADIM 1: printf() işlevleri\n\n");
 
 	printf("Adım 1.1: Gerekli printf() işlevleri\n");
-	printf("%%c: %c\n", 'a');
-	printf("%%s: %s\n", "Test");
-	printf("%%p: %p\n", (void *)"Test");
-	printf("%%d: %d\n", 100);
-	printf("%%i: %i\n", (int)0x80000000);
-	printf("%%u: %u\n", 0xFFFFFFFF);
-	printf("%%x: %x\n", 0xAAbbCCdd);
-	printf("%%X: %X\n", 0xa1b2c3d4);
-	printf("%%%%: %%\n");
-	//printf("heheehe%d%c%s %+010\n", 10, 'c', "a", 10);
+	res = printf("%%c: %c ", 'a');
+	printf("(returned: %i)\n", res);
+	res = printf("%%s: %s ", "Test");
+	printf("(returned: %i)\n", res);
+	res = printf("%%p: %p ", "Test");
+	printf("(returned: %i)\n", res);
+	res = printf("%%d: %d ", 100);
+	printf("(returned: %i)\n", res);
+	res = printf("%%d: %d ", -100);
+	printf("(returned: %i)\n", res);
+	res = printf("%%i: %i ", 100);
+	printf("(returned: %i)\n", res);
+	res = printf("%%i: %i ", -100);
+	printf("(returned: %i)\n", res);
+	res = printf("%%u: %u ", -1);
+	printf("(returned: %i)\n", res);
+	res = printf("%%x: %x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf("%%X: %X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf("%%%%: %% ");
+	printf("(returned: %i)\n", res);
 
 	printf("\nAdım 1.2: Bonus işlevler\n");
 
 	printf("\n1.2.1) Field Width öncesi flag'ler\n");
 
 	printf("\n1.2.1.1) '#' flag'i:\n");
-	//printf("%%#o: %#o %#o %#o\n", 0, 100, -100);
-	printf(" %%#x: %#x\n", 0xa1b2c3d4);
-	printf(" %%#X: %#X\n", 0xa1b2c3d4);
-	printf(" %%#x: %#12x\n", 0xa1b2c3d4);
-	printf(" %%#X: %#12X\n", 0xa1b2c3d4);
+	res = printf(" %%#x: %#x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%#X: %#X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	// printf(" %%#x: %#12x\n", 0xa1b2c3d4);
+	// printf(" %%#X: %#12X\n", 0xa1b2c3d4);
 
 	printf("\n1.2.1.2) '0' flag'i:\n");
-	printf(" %%0x: %010x\n", 0xa1b2c3d4);
-	printf(" %%0X: %010X\n", 0xa1b2c3d4);
-	printf(" %%0#x: %0#10x\n", 0xffffffff);
-	printf(" %%0#X: %0#12X\n", 0xa1b2c3d4);
-	printf(" %%#0x: %#012x\n", 0xa1b2c3d4);
-	printf(" %%#0X: %#012X\n", 0xa1b2c3d4);
+	res = printf(" %%0x: %010x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%0X: %010X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%0#x: %0#10x ", 0xffffffff);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%0#X: %0#12X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%#0x: %#012x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = printf(" %%#0X: %#012X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
 
 	printf("\n1.2.1.3) '-' flag'i:\n");
 	printf(" %%-x: %-10xtest\n", 0xa1b2c3d4);
@@ -47,44 +67,149 @@ int	main(void)
 
 	printf(" %%#-X: %.10s\n", "aabbccddffeegghh");
 
-	printf("\n\nADIM 2: ft_printf() testi ve karşılaştırması\n\n");
+	printf("\n\nADIM 2: ft_printf() Testi ve Karşılaştırmaları\n\n");
 
-	printf("Adım 2.1: Gerekli ft_printf() işlevleri\n\n");
+	printf("Adım 2.1: Gerekli ft_printf() İşlevleri\n\n");
 
-	printf("printf(%%): %%\n");
-	ft_printf("ft_printf(%%): %%\n\n");
+	res = printf("   printf(): Sade bir yazı ");
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(): Sade bir yazı ");
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%c): %c\n", 'c');
-	ft_printf("ft_printf(%%c): %c\n\n", 'c');
+	res = printf("   printf(%%): %% ");
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%): %% ");
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%s): %s\n", "hello");
-	ft_printf("ft_printf(%%s): %s\n\n", "hello");
+	res = printf("   printf(%%c): %c ", 'c');
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%c): %c ", 'c');
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%p): %20p\n", "hello");
-	ft_printf("ft_printf(%%p): %20p\n", "hello");
-	printf("printf(%%p): %20p\n", NULL);
-	ft_printf("ft_printf(%%p): %20p\n\n", NULL);
+	res = printf("   printf(%%s): %s ", "hello");
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%s): %s ", "hello");
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%d): %d\n", 1234567890);
-	ft_printf("ft_printf(%%d): %d\n", 1234567890);
-	printf("printf(%%d): %d\n", -1234567890);
-	ft_printf("ft_printf(%%d): %d\n\n", -1234567890);
+	res = printf("   printf(%%p): %p ", "hello");
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%p): %p ", "hello");
+	printf("(returned: %i)\n\n", res);
+	// printf("printf(%%p): %p\n", NULL);
+	// ft_printf("ft_printf(%%p): %p\n\n", NULL);
 
-	printf("printf(%%i): %i\n", 1234567890);
-	ft_printf("ft_printf(%%i): %i\n", 1234567890);
-	printf("printf(%%i): %i\n", -1234567890);
-	ft_printf("ft_printf(%%i): %i\n\n", -1234567890);
+	res = printf("   printf(%%d): %d ", 1234567890);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%d): %d ", 1234567890);
+	printf("(returned: %i)\n", res);
+	res = printf("   printf(%%d): %d ", -1234567890);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%d): %d ", -1234567890);
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%u): %u\n", -1);
-	ft_printf("ft_printf(%%u): %u\n\n", -1);
+	res = printf("   printf(%%i): %i ", 1234567890);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%i): %i ", 1234567890);
+	printf("(returned: %i)\n", res);
+	res = printf("   printf(%%i): %i ", -1234567890);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%i): %i ", -1234567890);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%u): %u ", -1);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%u): %u ", -1);
+	printf("(returned: %i)\n\n", res);
 	
-	printf("printf(%%x): %x\n", 0xabcdef);
-	ft_printf("ft_printf(%%x): %x\n\n", 0xabcdef);
+	res = printf("   printf(%%x): %x ", 0x1A2B3C4D);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%x): %x ", 0x1A2B3C4D);
+	printf("(returned: %i)\n\n", res);
 
-	printf("printf(%%X): %X\n", 0xabcdef);
-	ft_printf("ft_printf(%%X): %X\n\n", 0xabcdef);
+	res = printf("   printf(%%X): %X ", 0x1A2B3C4D);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%X): %X ", 0x1A2B3C4D);
+	printf("(returned: %i)\n\n", res);
 
-	printf("Adım 2.2: Bonus işlevler ve karşılaştırması\n\n");
+	printf("Adım 2.2: Bonus İşlevler ve Karşılaştırmalar\n\n");
+
+	printf("\n2.2.1) Field Width öncesi flag'ler\n");
+
+	printf("\n2.2.1.1) '#' flag'i:\n");
+	res = printf("   printf(%%#x): %#x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#x): %#x ", 0xa1b2c3d4);
+	ft_printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%#X) %#X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#X) %#X ", 0xa1b2c3d4);
+	ft_printf("(returned: %i)\n\n", res);
+
+	printf("\n2.2.1.2) '0' flag'i:\n");
+	res = printf("   printf(%%010x): %010x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%010x): %010x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%010X) %010X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%010X) %010X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%0#10x): %0#10x ", 0xffffffff);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%0#10x): %0#10x ", 0xffffffff);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%0#12X): %0#12X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%0#12X): %0#12X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%#012x): %#012x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#012x): %#012x ", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%#012X): %#012X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#012X): %#012X ", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	printf("\n2.2.1.3) '-' flag'i:\n");
+	res = printf("   printf(%%-10x): %-10x", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%-10x): %-10x", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%-10X): %-10X", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%-10X): %-10X", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%-12x): %-12s", "\"Test\"");
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%-12s): %-12s", "\"Test\"");
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%-#12X): %-#12X", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%-#12X): %-#12X", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%#-12x): %#-12x", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#-12x): %#-12x", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	res = printf("   printf(%%#-12X): %#-12X", 0xa1b2c3d4);
+	printf("(returned: %i)\n", res);
+	res = ft_printf("ft_printf(%%#-12X): %#-12X", 0xa1b2c3d4);
+	printf("(returned: %i)\n\n", res);
+
+	printf("Adım 2.3: Hata Testleri ve Sakat Durumlar\n\n");
+	printf("Adım 2.3.1: NULL testi\n\n");
 	
 	return (0);
 }
