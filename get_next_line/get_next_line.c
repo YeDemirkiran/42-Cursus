6a7c7f7fc8eb9d70d8dd2c07953792a0a2496b7f
YeDemirkiran <yedemirkiran27@gmail.com>
2025-06-20 17:03:18 +0300

    Init get_next_line project

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,17 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	if (fd < 0)
+		return (NULL);
+	return (NULL);
+}
+
+#include "stdio.h"
+#include "fcntl.h"
+
+int main(void)
+{
+	printf("Buffer size: %i\n", BUFFER_SIZE);
+	return (0);
+}
