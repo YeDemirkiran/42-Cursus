75d0a445f9b0e81f6a651f08756580263caddc84
YeDemirkiran <yedemirkiran27@gmail.com>
2025-06-20 16:35:18 +0300

    .

--- ft_printf/test/main.c
+++ ft_printf/test/main.c
@@ -3,11 +3,7 @@
 
 int	main(void)
 {
-	int	res;
-	res = printf("%#014x", 22);
-	printf(" (returned: %i)\n", res);
-	res = ft_printf("%#014x", 22);
-	printf(" (returned: %i)\n", res);
+	
 
 	return (0);
 }
