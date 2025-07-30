cfd4b61d4a54927e6166b331c58101e3ab56a140
YeDemirkiran <yedemirkiran27@gmail.com>
2025-07-30 23:27:48 +0300

    Complete the printing part of the project

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -15,6 +15,22 @@ typedef struct s_stack
 	int	index;
 }			t_stack;
 
+enum e_instructions 
+{
+	INST_END = 0,
+	INST_SWAP_A = 1,
+	INST_SWAP_B = 2,
+	INST_SWAP_AB = 3,
+	INST_PUSH_A = 4,
+	INST_PUSH_B = 5,
+	INST_ROT_A = 6,
+	INST_ROT_B = 7,
+	INST_ROT_AB = 8,
+	INST_RROT_A = 9,
+	INST_RROT_B = 10,
+	INST_RROT_AB = 11,
+};
+
 t_stack	*init_stack(char **arr, int argc);
 
 #endif
--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -1,5 +1,44 @@
 #include "push_swap.h"
 
+int	*get_instructions(t_stack *stack, int size)
+{
+	int	*instructions;
+
+	instructions = malloc(sizeof(int) * 1024);
+	(void)stack;
+	(void)size;
+	// Analyze and sort the stack
+	// Fill the instructions
+	instructions[0] = INST_SWAP_A;
+	instructions[1] = INST_SWAP_B;
+	instructions[2] = INST_SWAP_AB;
+	instructions[3] = INST_PUSH_A;
+	instructions[4] = INST_PUSH_B;
+	instructions[5] = INST_ROT_A;
+	instructions[6] = INST_ROT_B;
+	instructions[7] = INST_ROT_AB;
+	instructions[8] = INST_RROT_A;
+	instructions[9] = INST_RROT_B;
+	instructions[10] = INST_RROT_AB;
+	instructions[11] = INST_END;
+	return (instructions);
+}
+
+void	print_instructions(int *instructions)
+{
+	int	i;
+	const char	*inst[] = {NULL, "sa", "sb", "ss", "pa", "pb",
+				"ra", "rb", "rr", "rra", "rrb", "rrr"};
+
+	i = 0;
+	while (instructions[i])
+	{
+		ft_putstr((char *)inst[instructions[i]]);
+		ft_putchar('\n');
+		i++;
+	}	
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*stack;
@@ -13,14 +52,14 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Error\n", 2);
 		return(EXIT_FAILURE);
 	}
-	// instructions = get_instructions(stack, argc - 1);
-	// if (!instructions)
-	// 		fail
-	// print_instructions(instructions);
-	// free(instructions);
-	(void)stack;
-	(void)instructions;
+	instructions = get_instructions(stack, argc - 1);
+	if (!instructions)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return(EXIT_FAILURE);
+	}
+	print_instructions(instructions);
+	free(instructions);
 	free(stack);
-	ft_putstr_fd("pa\n", 1);
 	return (EXIT_SUCCESS);
 }
