7c4055fb8c668aae191cbada992137c855ecbc28
Yaşam Ensar Demirkıran <yademirk@student.42istanbul.com.tr>
2025-08-01 17:12:52 +0300

    Add s_image struct

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 13:02:50 by yademirk          #+#    #+#             */
-/*   Updated: 2025/08/01 13:59:51 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/08/01 16:56:36 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,28 @@
 
 # include "math.h"
 # include "mlx.h"
+# include "stddef.h"
+# include "stdlib.h"
+
+# ifndef WINDOW_TITLE
+#  define WINDOW_TITLE "So Long"
+# endif
+
+# ifndef RES_X
+#  define RES_X 1920
+# endif
+
+# ifndef RES_Y
+#  define RES_Y 1080
+# endif
+
+typedef struct s_image
+{
+	void	*img_addr;
+	char	*mod_addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_image;
 
 #endif
--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,19 +6,51 @@
 /*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 13:02:55 by yademirk          #+#    #+#             */
-/*   Updated: 2025/08/01 16:05:47 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/08/01 17:12:26 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "stdio.h"
 
+void	ft_put_pixel(t_image image, int pos_x, int pos_y, int color)
+{
+	char	*dst;
+
+	dst = image.mod_addr + (pos_y * image.line_length + pos_x * (image.bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
 int	main(int argc, char **argv)
 {
 	void	*mlx_addr;
-	
+	void	*mlx_window;
+	t_image	mlx_image;
+	int		x;
+	int		y;
+
 	(void)argc;
 	(void)argv;
 	mlx_addr = mlx_init();
-	return (0);
+	if (!mlx_addr)
+		return (EXIT_FAILURE);
+	mlx_window = mlx_new_window(mlx_addr, RES_X, RES_Y, WINDOW_TITLE);
+	if (!mlx_window)
+		return (EXIT_FAILURE);
+	mlx_image.img_addr = mlx_new_image(mlx_addr, 200, 500);
+	mlx_image.mod_addr = mlx_get_data_addr(mlx_image.img_addr, &mlx_image.bits_per_pixel, &mlx_image.line_length, &mlx_image.endian);
+	x = 0;
+	while (x < 200)
+	{
+		y = 0;
+		while (y < 500)
+		{
+			ft_put_pixel(mlx_image, x, y, 0x00FFFFFF);
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(mlx_addr, mlx_window, mlx_image.img_addr, RES_X / 2, RES_Y / 2);
+	mlx_loop(mlx_addr);
+	return (EXIT_SUCCESS);
 }
