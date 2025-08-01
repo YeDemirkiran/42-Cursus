d981459357328125f28526f3edc393c8b0107409
Yaşam Ensar Demirkıran <yademirk@student.42istanbul.com.tr>
2025-08-01 20:13:22 +0300

    .

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 13:02:55 by yademirk          #+#    #+#             */
-/*   Updated: 2025/08/01 17:12:26 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/08/01 17:15:41 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,20 @@ int	main(int argc, char **argv)
 	mlx_window = mlx_new_window(mlx_addr, RES_X, RES_Y, WINDOW_TITLE);
 	if (!mlx_window)
 		return (EXIT_FAILURE);
-	mlx_image.img_addr = mlx_new_image(mlx_addr, 200, 500);
+	mlx_image.img_addr = mlx_new_image(mlx_addr, RES_X, RES_Y);
 	mlx_image.mod_addr = mlx_get_data_addr(mlx_image.img_addr, &mlx_image.bits_per_pixel, &mlx_image.line_length, &mlx_image.endian);
 	x = 0;
-	while (x < 200)
+	while (x < RES_X)
 	{
 		y = 0;
-		while (y < 500)
+		while (y < RES_Y)
 		{
 			ft_put_pixel(mlx_image, x, y, 0x00FFFFFF);
 			y++;
 		}
 		x++;
 	}
-	mlx_put_image_to_window(mlx_addr, mlx_window, mlx_image.img_addr, RES_X / 2, RES_Y / 2);
+	mlx_put_image_to_window(mlx_addr, mlx_window, mlx_image.img_addr, 0, 0);
 	mlx_loop(mlx_addr);
 	return (EXIT_SUCCESS);
 }
