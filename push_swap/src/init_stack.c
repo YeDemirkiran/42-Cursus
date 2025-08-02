f9e231591be8e706d407c08f15aa6d7406be76c1
Yaşam Ensar Demirkıran <yademirk@student.42istanbul.com.tr>
2025-08-02 12:32:49 +0300

    Refactor stack functions

--- push_swap/src/init_stack.c
+++ push_swap/src/init_stack.c
@@ -6,14 +6,14 @@
 /*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 11:58:01 by yademirk          #+#    #+#             */
-/*   Updated: 2025/08/02 11:58:02 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/08/02 12:32:15 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 //#include "stdio.h"
 
-static int is_duplicate(t_stack *stack, int index)
+static int	is_duplicate(t_stack *stack, int index)
 {
 	int	i;
 	int	j;
@@ -33,12 +33,38 @@ static int is_duplicate(t_stack *stack, int index)
 	return (TRUE);
 }
 
+static t_stack_pair	free_stack(t_stack *stack)
+{
+	t_stack_pair	empty;
+
+	empty.stack_a = NULL;
+	empty.stack_b = NULL;
+	empty.a_length = 0;
+	empty.b_length = 0;
+	if (stack)
+		free(stack);
+	return (empty);
+}
+
+static void	init_stack_empty(t_stack *stack, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		stack[i].number = 0;
+		stack[i].index = i;
+		i++;
+	}
+}
+
 t_stack_pair	init_stack_pair(char **arr, int argc)
 {
 	int				i;
 	t_stack_pair	pair;
 	long long		num;
-	
+
 	i = 0;
 	pair.stack_a = malloc(sizeof(t_stack) * argc);
 	if (!pair.stack_a)
@@ -46,38 +72,20 @@ t_stack_pair	init_stack_pair(char **arr, int argc)
 	while (i < argc)
 	{
 		if (!ft_isnumber(arr[i]))
-		{
-			free(pair.stack_a);
-			return (pair);
-		}
+			return (free_stack(pair.stack_a));
 		num = ft_atol(arr[i]);
 		if (num > INT_MAX || num < INT_MIN)
-		{
-			free(pair.stack_a);
-			return (pair);
-		}
+			return (free_stack(pair.stack_a));
 		pair.stack_a[i].index = i;
 		pair.stack_a[i].number = (int)num;
 		if (!is_duplicate(pair.stack_a, i))
-		{
-			free(pair.stack_a);
-			return (pair);
-		}
+			return (free_stack(pair.stack_a));
 		i++;
 	}
 	pair.stack_b = malloc(sizeof(t_stack) * argc);
 	if (!pair.stack_b)
-	{
-		free(pair.stack_a);
-		return (pair);
-	}
-	i = 0;
-	while (i < argc)
-	{
-		pair.stack_b[i].number = 0;
-		pair.stack_b[i].index = i;
-		i++;
-	}
+		return (free_stack(pair.stack_a));
+	init_stack_empty(pair.stack_b, argc);
 	pair.a_length = argc;
 	pair.b_length = 0;
 	return (pair);
