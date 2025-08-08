78c71e31aec03f5b172bffe1e1d965cf4e3f936d
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-08 21:47:08 +0300

    Add reading from stdin for the second comman d

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -68,7 +68,8 @@ int	main(int argc, char **argv, char **envp)
 {
 	char	*program_path;
 	int		execve_result;
-	int		fds[2];
+	int		pipe_1[2];
+	int		pipe_2[2];
 	int		fd_tmp;
 	size_t	read_size;
 	char	*buffer;
@@ -79,8 +80,14 @@ int	main(int argc, char **argv, char **envp)
 		return (EXIT_FAILURE);
 	if (access(argv[1], R_OK) == -1)
 		return (EXIT_FAILURE);
-	if (pipe(fds) == -1)
+	if (pipe(pipe_1) == -1)
 		return (EXIT_FAILURE);
+	if (pipe(pipe_2) == -1)
+	{
+		close(pipe_1[0]);
+		close(pipe_1[1]);
+		return (EXIT_FAILURE);
+	}
 	pid[0] = fork();
 	cmd_args = NULL;
 	if (pid[0] < 0)
@@ -90,19 +97,19 @@ int	main(int argc, char **argv, char **envp)
 	}
 	else if (pid[0] == 0)
 	{
-		close(fds[1]);
+		close(pipe_1[1]);
+		close(pipe_2[0]);
 		cmd_args = ft_split(argv[2], ' ');
 		if (!cmd_args)
 			exit(EXIT_FAILURE);
-		//printf("CMD: %s\n", cmd_args[0]);
 		if (cmd_args[0][0] == '.' || cmd_args[0][0] == '/')
 			program_path = ft_strdup(cmd_args[0]);
 		else
 			program_path = find_path_in_envp(cmd_args[0], envp);
-		//printf("PROGRAM: %s\n", program_path);
-		//printf("ARG 1: %s\n", *(cmd_args + 1));
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[0]);
+		dup2(pipe_1[0], STDIN_FILENO);
+		dup2(pipe_2[1], STDOUT_FILENO);
+		close(pipe_1[0]);
+		close(pipe_2[1]);
 		execve_result = execve(program_path, cmd_args, envp);
 		if (execve_result == -1)
 		{
@@ -113,7 +120,7 @@ int	main(int argc, char **argv, char **envp)
 	}
 	else
 	{
-		close(fds[0]);
+		close(pipe_1[0]);
 		fd_tmp = open(argv[1], O_RDONLY);
 		buffer = malloc(sizeof(char) * (size_t)BUFFER_SIZE);
 		if (fd_tmp < 0 || !buffer)
@@ -121,34 +128,42 @@ int	main(int argc, char **argv, char **envp)
 		read_size = read(fd_tmp, buffer, (size_t)BUFFER_SIZE);
 		while (read_size > 0)
 		{
-			write(fds[1], buffer, read_size);
+			write(pipe_1[1], buffer, read_size);
 			read_size = read(fd_tmp, buffer, (size_t)BUFFER_SIZE);
 		}
-		close(fds[1]);
+		close(pipe_1[1]);
 		free(buffer);
 	}
-	// pid[1] = fork();
-	// if (pid < 0)
-	// {
-	// 	perror(argv[1]);
-	// 	exit(EXIT_FAILURE);
-	// }
-	// else if (pid[1] == 0)
-	// {
-	// 	free(cmd_args);
-	// 	cmd_args = ft_split(argv[3], ' ');
-	// 	if (!cmd_args)
-	// 		exit(EXIT_FAILURE);
-	// 	program_path = find_path_in_envp(cmd_args[0], envp);
-	// 	execve_result = execve(program_path, cmd_args + 1, envp);
-	// 	if (execve_result == -1)
-	// 	{
-	// 		perror(cmd_args[0]);
-	// 		free(program_path);
-	// 		exit(EXIT_FAILURE);
-	// 	}
-	// }
+	pid[1] = fork();
+	if (pid[1] < 0)
+	{
+		perror(argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid[1] == 0)
+	{
+		close(pipe_2[1]);
+		free(cmd_args);
+		cmd_args = ft_split(argv[3], ' ');
+		if (!cmd_args)
+			exit(EXIT_FAILURE);
+		program_path = find_path_in_envp(cmd_args[0], envp);
+		dup2(pipe_2[0], STDIN_FILENO);
+		close(pipe_2[0]);
+		execve_result = execve(program_path, cmd_args, envp);
+		if (execve_result == -1)
+		{
+			perror(cmd_args[0]);
+			free(program_path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		close(pipe_2[0]);
+		close(pipe_2[1]);
+	}
 	waitpid(pid[0], NULL, 0);
-	//waitpid(pid[1], NULL, 0);
+	waitpid(pid[1], NULL, 0);
 	return (EXIT_SUCCESS);
 }
