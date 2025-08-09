9d607e6df31a214e13e88885f16c275398b0f2c3
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-09 23:34:55 +0300

    Add child process function

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -12,24 +12,10 @@
 
 #include "pipex.h"
 
-static int	print_strerror(char *program_name)
-{
-	perror(program_name);
-	return (EXIT_FAILURE);
-}
-
 static void	strerror_exit(char *perror_str)
 {
 	perror(perror_str);
 	exit(EXIT_FAILURE);
-}
-
-static void prepare_vars(char ***cmd_args, char **buffer)
-{
-	*cmd_args = NULL;
-	*buffer = malloc(sizeof(char) * (size_t)BUFFER_SIZE);
-	if (!*buffer)
-		strerror_exit("pipex");
 }
 
 static void	prepare_files(char *input_file_path, char *output_file_path, int *io_fd)
@@ -51,73 +37,114 @@ static void	prepare_files(char *input_file_path, char *output_file_path, int *io
 		strerror_exit("pipex: open");
 }
 
+static void	set_fds(t_fd_info fd_info)
+{
+	int	i;
+
+	i = 0;
+	while (fd_info.fds_to_close_immediately && fd_info.fds_to_close_immediately[i] != -1)
+		close(fd_info.fds_to_close_immediately[i++]);
+	if (fd_info.stdin_fd >= 0)
+	{
+		dup2(fd_info.stdin_fd, STDIN_FILENO);
+		close(fd_info.stdin_fd);
+	}
+	if (fd_info.stdout_fd >= 0)
+	{
+		dup2(fd_info.stdout_fd, STDOUT_FILENO);
+		close(fd_info.stdout_fd);
+	}
+}
+
+static void	create_child_process(int *pid_save, char *program_name, char *program_args, char **envp, t_fd_info fd_info)
+{
+	int		pid;
+	char	**cmd_args;
+	char	*program_path;
+
+	pid = fork();
+	if (pid < 0)
+		strerror_exit(program_name);
+	else if (pid == 0)
+	{
+		set_fds(fd_info);
+		cmd_args = ft_split(program_args, ' ');
+		if (!cmd_args)
+			strerror_exit(cmd_args[0]);
+		program_path = parse_program_path(cmd_args[0], envp);	
+		execve(program_path, cmd_args, envp);
+		free(program_path);
+		strerror_exit(cmd_args[0]);
+	}
+	*pid_save = pid;
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char	*program_path;
-	int		execve_result;
-	int		pipes[3][2];
-	int		io_fd[2];
-	char	*buffer;
-	char	**cmd_args;
-	pid_t	pid[2];
+	int			pipes[3][2];
+	int			io_fd[2];
+	pid_t		pid[2];
+	t_fd_info	fd_info;
 
 	if (argc <= 1)
 		return (EXIT_FAILURE);
-	prepare_vars(&cmd_args, &buffer);
 	prepare_files(argv[1], argv[4], io_fd);
 	if (pipe(pipes[0]) == -1)
-		return (print_strerror(argv[0]));
-	pid[0] = fork();
-	if (pid[0] < 0)
-		return (print_strerror(argv[0]));
-	else if (pid[0] == 0)
-	{
-		close(pipes[0][0]);
-		cmd_args = ft_split(argv[2], ' ');
-		if (!cmd_args)
-			strerror_exit(cmd_args[0]);
-		program_path = parse_program_path(cmd_args[0], envp);
-		dup2(pipes[0][1], STDOUT_FILENO);
-		close(pipes[0][1]);
-		execve_result = execve(program_path, cmd_args, envp);
-		if (execve_result == -1)
-		{
-			free(program_path);
-			strerror_exit(cmd_args[0]);
-		}
-	}
-	else
-		close(pipes[0][1]);
+		strerror_exit(argv[0]);
+	fd_info.fds_to_close_immediately = malloc(sizeof(int) * 2);
+	fd_info.fds_to_close_immediately[0] = pipes[0][0];
+	fd_info.fds_to_close_immediately[1] = -1;
+	fd_info.stdin_fd = -1;
+	fd_info.stdout_fd = pipes[0][1];
+	create_child_process(pid, argv[0], argv[2], envp, fd_info);
+	// pid[0] = fork();
+	// if (pid[0] < 0)
+	// 	strerror_exit(argv[0]);
+	// else if (pid[0] == 0)
+	// {
+	// 	close(pipes[0][0]);
+	// 	cmd_args = ft_split(argv[2], ' ');
+	// 	if (!cmd_args)
+	// 		strerror_exit(cmd_args[0]);
+	// 	program_path = parse_program_path(cmd_args[0], envp);
+	// 	dup2(pipes[0][1], STDOUT_FILENO);
+	// 	close(pipes[0][1]);
+	// 	execve_result = execve(program_path, cmd_args, envp);
+	// 	if (execve_result == -1)
+	// 	{
+	// 		free(program_path);
+	// 		strerror_exit(cmd_args[0]);
+	// 	}
+	// }
+	close(pipes[0][1]);
+	free(fd_info.fds_to_close_immediately);
+	fd_info.fds_to_close_immediately = NULL;
+	fd_info.stdin_fd = pipes[0][0];
+	fd_info.stdout_fd = io_fd[1];
+	create_child_process(pid + 1, argv[0], argv[3], envp, fd_info);
 	pid[1] = fork();
-	if (pid[1] < 0)
-	{
-		perror(argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	else if (pid[1] == 0)
-	{
-		cmd_args = ft_split(argv[3], ' ');
-		if (!cmd_args)
-			strerror_exit(cmd_args[0]);
-		program_path = parse_program_path(cmd_args[0], envp);
-		dup2(pipes[0][0], STDIN_FILENO);
-		dup2(io_fd[1], STDOUT_FILENO);
-		close(pipes[0][0]);
-		close(io_fd[1]);
-		execve_result = execve(program_path, cmd_args, envp);
-		if (execve_result == -1)
-		{
-			free(program_path);
-			strerror_exit(cmd_args[0]);
-		}
-	}
-	else
-	{
-		close(pipes[0][0]);
-		close(io_fd[1]);
-	}
+	// if (pid[1] < 0)
+	// 	strerror_exit(argv[0]);
+	// else if (pid[1] == 0)
+	// {
+	// 	cmd_args = ft_split(argv[3], ' ');
+	// 	if (!cmd_args)
+	// 		strerror_exit(cmd_args[0]);
+	// 	program_path = parse_program_path(cmd_args[0], envp);
+	// 	dup2(pipes[0][0], STDIN_FILENO);
+	// 	dup2(io_fd[1], STDOUT_FILENO);
+	// 	close(pipes[0][0]);
+	// 	close(io_fd[1]);
+	// 	execve_result = execve(program_path, cmd_args, envp);
+	// 	if (execve_result == -1)
+	// 	{
+	// 		free(program_path);
+	// 		strerror_exit(cmd_args[0]);
+	// 	}
+	// }
+	close(pipes[0][0]);
+	close(io_fd[1]);
 	waitpid(pid[0], NULL, 0);
 	waitpid(pid[1], NULL, 0);
-	free(buffer);
 	return (EXIT_SUCCESS);
 }
