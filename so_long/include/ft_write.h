8c73ef61028bfd3ba5a98fd1b693a1b5e2aafc13
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-17 22:19:13 +0300

    Write move count

--- so_long/include/ft_write.h
+++ so_long/include/ft_write.h
@@ -2,10 +2,13 @@
 # define FT_WRITE_H
 
 # include "unistd.h"
+# include "ft_string.h"
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putchar(char c);
 void	ft_putstr_fd(char *str, int fd);
 void	ft_putstr(char *str);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
 
 #endif
--- so_long/src/update.c
+++ so_long/src/update.c
@@ -1,19 +1,38 @@
 #include "so_long.h"
+#include "ft_write.h"
 
 void	update_player(t_player *player, t_frame *frame)
 {
 	t_vec_2	overlap;
+	int		count_x;
+	int		count_y;
 
 	player->object.position.x += player->object.velocity.x;
 	player->object.position.y += player->object.velocity.y;
+	count_x = player->object.velocity.x != 0;
+	count_y = player->object.velocity.y != 0;
 	overlap = player_overlapping_wall(frame);
 	if (overlap.x && overlap.y)
 	{
 		if (fabs(overlap.x) < fabs(overlap.y))
+		{
 			player->object.position.x += overlap.x;
+			count_x = 0;
+		}
 		else
+		{
 			player->object.position.y += overlap.y;
+			count_y = 0;
+		}
 	}
+	if (count_x || count_y)
+	{
+		player->move_count += count_x + count_y;
+		ft_putstr("PLAYER MOVE COUNT: ");
+		ft_putnbr(player->move_count);
+		ft_putstr("\n");	
+	}
+	
 }
 
 void	update_camera_offset(t_frame *frame)
