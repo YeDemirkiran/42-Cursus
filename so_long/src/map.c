0c87c4cddef659509d4ba178f73d78d641bcce4b
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-18 23:20:46 +0300

    First step of parsing map

--- so_long/src/map.c
+++ so_long/src/map.c
@@ -1,4 +1,6 @@
 #include "so_long.h"
+#include "stdio.h"
+
 
 // STEP 1: Save map to a 2D buffer array
 // STEP 2: Check if the map is valid (rectangular, enclosed by walls, start, exit)
@@ -21,41 +23,41 @@
 // 	return (1);
 // }
 
-// static int	read_line(char *line, int line_y, t_map *map, t_frame *frame)
-// {
-// 	int		i;
-// 	t_vec_2	pos;
+static int	read_line(char *line, int line_y, t_map *map, t_frame *frame)
+{
+	int		i;
+	t_vec_2	pos;
 
-// 	if (!check_map_width(line, map))
-// 		return (0);
-// 	i = 0;
-// 	while (line[i] && line[i] != '\n')
-// 	{
-// 		if (line[i] == OBJ_WALL_CHAR)
-// 		{
-// 			pos.x = i * frame->sprites[S_WALL].size.x;
-// 			pos.y = line_y * frame->sprites[S_WALL].size.y;
-// 			add_wall(frame, pos);
-// 		}
-// 		else if (line[i] == OBJ_COLL_CHAR)
-// 		{
+	i = 0;
+	while (line[i])
+	{
+		//printf("Current map char: %c\n", line[i]);
+		if (line[i] == OBJ_WALL_CHAR)
+		{
+			pos.x = i * frame->sprites[S_WALL].size.x;
+			pos.y = line_y * frame->sprites[S_WALL].size.y;
+			add_wall(frame, pos);
+		}
+		else if (line[i] == OBJ_COLL_CHAR)
+		{
 			
-// 		}
-// 		else if (line[i] == OBJ_EXIT_CHAR)
-// 		{
-// 			map->exit_pos.x = i * frame->sprites[S_EXIT].size.x;
-// 			map->exit_pos.y = line_y * frame->sprites[S_EXIT].size.y;
-// 		}
-// 		else if (line[i] == OBJ_START_CHAR)
-// 		{
-// 			map->start_pos.x = i * frame->sprites[S_PLAYER].size.x;
-// 			map->start_pos.y = line_y * frame->sprites[S_PLAYER].size.y;
-// 		}
-// 		else if (line[i] != OBJ_EMPTY_CHAR)
-// 			exit(EXIT_FAILURE);
-// 		i++;
-// 	}
-// }
+		}
+		else if (line[i] == OBJ_EXIT_CHAR)
+		{
+			map->exit_pos.x = i * frame->sprites[S_EXIT].size.x;
+			map->exit_pos.y = line_y * frame->sprites[S_EXIT].size.y;
+		}
+		else if (line[i] == OBJ_START_CHAR)
+		{
+			map->start_pos.x = i * frame->sprites[S_PLAYER].size.x;
+			map->start_pos.y = line_y * frame->sprites[S_PLAYER].size.y;
+		}
+		else if (line[i] != OBJ_EMPTY_CHAR)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 // static t_map	read_map(char *path, t_frame *frame)
 // {
@@ -95,7 +97,6 @@ char	**save_map(char *path, t_map *map)
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		return (NULL);
-	map->map_width = -1;
 	map->map_height = 0;
 	buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	if (!buffer)
@@ -108,17 +109,13 @@ char	**save_map(char *path, t_map *map)
 		while (buffer[i])
 		{
 			if (buffer[i] == '\n')
-			{
-				if (map->map_width == -1)
-					map->map_width = i;
 				map->map_height++;
-			}
 			i++;
 		}
 		read_size = read(fd, buffer, BUFFER_SIZE - 1);
 	}
 	free(buffer);
-	if (map->map_width == -1 || map->map_height == 0)
+	if (map->map_height == 0)
 		return (NULL);
 	map_buff = malloc(sizeof(char *) * (map->map_height + 1));
 	if (!map_buff)
@@ -136,6 +133,9 @@ char	**save_map(char *path, t_map *map)
 	i = 0;
 	while (buffer)
 	{
+		read_size = ft_strlen(buffer);
+		if (buffer[read_size - 1] == '\n')
+			buffer[read_size - 1] = 0;
 		map_buff[i++] = buffer;
 		buffer = get_next_line(fd);
 	}
@@ -143,18 +143,69 @@ char	**save_map(char *path, t_map *map)
 	return (map_buff);
 }
 
-#include "stdio.h"
+
+
+int	is_full_wall(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != OBJ_WALL_CHAR)
+		{
+			printf("%i is not a wall\n", str[i]);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	is_enclosed_wall(char *str)
+{
+	size_t	len;
+
+	len = ft_strlen(str);
+	return (str[0] == OBJ_WALL_CHAR && str[len - 1] == OBJ_WALL_CHAR);
+}
+
 
 t_map	parse_map(char *path, t_frame *frame)
 {
 	t_map	map;
 	char	**map_buff;
+	int		i;
 
-	(void)frame;
 	map.map_valid = 0;
 	map_buff = save_map(path, &map);
 	if (!map_buff)
+	{
+		printf("Couldn't save the map.\n");
 		return (map);
+	}
+	i = 0;
+	while (map_buff[i])
+	{
+		if ((i == 0 || map_buff[i + 1] == NULL) && !is_full_wall(map_buff[i]))
+		{
+			printf("First or last not a full wall\n");
+			break;
+		}
+		if (i == 0)
+			map.map_width = ft_strlen(map_buff[i]);
+		else if (map.map_width != (int)ft_strlen(map_buff[i]) || !is_enclosed_wall(map_buff[i]))
+			break;
+		if (!read_line(map_buff[i], i, &map, frame))
+			break ;
+		i++;
+	}
+	if (map_buff[i])
+	{
+		printf("Map is not valid.\n");
+		return (map);
+	}
+	printf("Map is valid so far\n");
 	// int		i;
 	// int		j;
 	// int		tmp;
@@ -213,5 +264,6 @@ t_map	parse_map(char *path, t_frame *frame)
 	// }
 	// if (map.map_width == -1)
 	// 	exit(EXIT_FAILURE);
+	map.map_valid = 1;
 	return (map);
 }
