5973d021ad2f6fc0f3e278cac34c3e30812786e2
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-18 11:50:14 +0300

    Add back the cross movement

--- so_long/src/events.c
+++ so_long/src/events.c
@@ -18,11 +18,11 @@ static void	on_move(unsigned int key, t_player *player)
 	if (input_x)
 	{
 		player->object.velocity.x = input_x * PLAYER_SPEED;
-		player->object.velocity.y = 0;
+		//player->object.velocity.y = 0;
 	}
 	else if (input_y)
 	{
-		player->object.velocity.x = 0;
+		//player->object.velocity.x = 0;
 		player->object.velocity.y = input_y * PLAYER_SPEED;
 	}
 	if (input_x || input_y)
