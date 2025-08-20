c2af810f107dbff774d04da16eb524562e03d5f1
Yaşam Ensar Demirkıran <yademirk@student.42istanbul.com.tr>
2025-08-20 13:35:16 +0300

    Seperate main() for norm reasons

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -6,42 +6,54 @@
 /*   By: yademirk <yademirk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 13:02:55 by yademirk          #+#    #+#             */
-/*   Updated: 2025/08/20 11:45:55 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/08/20 13:34:35 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include "stdio.h"
+#include "ft_write.h"
+
+static int	initialize_mlx(t_frame *frame)
+{
+	frame->mlx_addr = mlx_init();
+	if (!frame->mlx_addr)
+		return (0);
+	frame->mlx_window = mlx_new_window(frame->mlx_addr,
+			RES_X, RES_Y, WINDOW_TITLE);
+	if (!frame->mlx_window)
+	{
+		mlx_destroy_display(frame->mlx_addr);
+		return (0);
+	}
+	frame->camera_offset.x = 0;
+	frame->camera_offset.y = 0;
+	init_sprites(frame->sprites, frame->mlx_addr);
+	init_background(frame);
+	init_objects(frame);
+	return (1);
+}
+
+static void	error_exit(char *msg)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(msg, 2);
+	exit(EXIT_FAILURE);
+}
 
 int	main(int argc, char **argv)
 {
 	t_frame	frame;
 	t_map	map;
 
-	if (argc != 2)
-	{
-		write(2, "Error\n", 6);
-		write(2, "Too few or too much arguments!\n", 31);
+	if (argc <= 1)
+		error_exit("No maps provided!\n");
+	else if (argc > 2)
+		error_exit("Too many arguments!\n");
+	if (!initialize_mlx(&frame))
 		exit(EXIT_FAILURE);
-	}
-	frame.mlx_addr = mlx_init();
-	if (!frame.mlx_addr)
-		return (EXIT_FAILURE);
-	frame.mlx_window = mlx_new_window(frame.mlx_addr, RES_X, RES_Y, WINDOW_TITLE);
-	if (!frame.mlx_window)
-		return (EXIT_FAILURE);
-	frame.camera_offset.x = 0;
-	frame.camera_offset.y = 0;
-	init_sprites(frame.sprites, frame.mlx_addr);
-	init_background(&frame);
-	init_objects(&frame);
 	map = parse_map(argv[1], &frame);
 	if (!map.map_valid)
-	{
-		write(2, "Error\n", 6);
-		write(2, "The given map is not valid!\n", 28);
-		return (EXIT_FAILURE);
-	}
+		error_exit("The given map is not valid!\n");
 	frame.map = &map;
 	init_player(&frame);
 	init_exit(&frame);
@@ -51,6 +63,5 @@ int	main(int argc, char **argv)
 	mlx_do_key_autorepeaton(frame.mlx_addr);
 	mlx_destroy_window(frame.mlx_addr, frame.mlx_window);
 	mlx_destroy_display(frame.mlx_addr);
-	printf("End.\n");
 	return (EXIT_SUCCESS);
 }
