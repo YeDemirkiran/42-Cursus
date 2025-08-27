94f00f480924fd542ebea2bfd001a6842b46ec40
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-27 13:31:42 +0300

    Some cleanup

--- push_swap/src/stack/stack_get.c
+++ push_swap/src/stack/stack_get.c
@@ -40,13 +40,9 @@ t_stack	get_place_before(t_stack *stack, int size, int target_num)
 {
 	int		i;
 	t_stack	stack_1;
-	//t_stack	stack_2;
 
-	//printf("NUM %i\n", target_num);
-	//fflush(stdout);
 	stack_1.index = -1;
 	stack_1.number = -1;
-	//stack_2.index = -1;
 	i = 1;
 	while (i < size)
 	{
@@ -56,37 +52,7 @@ t_stack	get_place_before(t_stack *stack, int size, int target_num)
 		}
 		i++;
 	}
-	//printf("TARGET %i\n", stack_1.number);
-	//fflush(stdout);
 	return (stack_1);
-	// i = size - 2;
-	// while (i >= size / 2)
-	// {
-	// 	if (stack[i].number > target_num && stack[i + 1].number < target_num)
-	// 	{
-	// 		stack_2 = stack[i];
-	// 		break ;
-	// 	}
-	// 	i--;
-	// }
-	// //printf("S1: %i\n", stack_1.index);
-	// //printf("S2: %i\n", stack_2.index);
-	// if (stack_2.index != -1 && (stack_1.index >= size - stack_2.index))
-	// {
-	// 	//printf("BOZO DE Mİ LOLO\n");
-
-	// 	return (stack_2);
-
-	// }
-	// else if (stack_1.index != -1 && (stack_2.index >= size - stack_1.index))
-	// {
-	// 	//printf("BAZA DE Mİ LOLO\n");
-	// 	return (stack_1);
-
-	// }
-	// //printf("BİZE DE Mİ LOLO\n");
-	// //fflush(stdout);
-	// return (stack[size - 1]);
 }
 
 static t_stack	get_between_last_half(t_stack *stack,
