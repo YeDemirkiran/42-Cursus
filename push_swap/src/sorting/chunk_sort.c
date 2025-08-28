c40d79caeab9d2cd264c406b0205d148693e3086
YeDemirkiran <yedemirkiran27@gmail.com>
2025-08-28 22:48:58 +0300

    Fix some norm errors

--- push_swap/src/sorting/chunk_sort.c
+++ push_swap/src/sorting/chunk_sort.c
@@ -14,10 +14,10 @@
 
 static void	push_chunks(t_stack_pair *pair, t_instructions *instructions)
 {
-	int			chunk_border;
-	int	current_border;
-	int			i;
-	t_stack		tmp;
+	int		chunk_border;
+	int		current_border;
+	int		i;
+	t_stack	tmp;
 
 	chunk_border = pair->full_length / CHUNK_SIZE;
 	current_border = 0;
@@ -26,7 +26,8 @@ static void	push_chunks(t_stack_pair *pair, t_instructions *instructions)
 		i = 0;
 		while (i < chunk_border)
 		{
-			if (current_border == CHUNK_SIZE && i >= pair->a_length % CHUNK_SIZE)
+			if (current_border == CHUNK_SIZE
+				&& i >= pair->a_length % CHUNK_SIZE)
 				break ;
 			tmp = get_between(pair->stack_a, pair->a_length,
 					chunk_border * current_border,
@@ -37,39 +38,55 @@ static void	push_chunks(t_stack_pair *pair, t_instructions *instructions)
 		}
 		current_border++;
 	}
-	// while (!is_stack_sorted(pair->stack_a, pair->a_length))
-	// {
-	// 	if (pair->stack_a[0].number > pair->stack_a[1].number && pair->stack_a[0].number > pair->stack_a[2].number)
-	// 		instructions->arr[instructions->index++] = stack_swap_a(*pair);
-	// 	else if (pair->stack_a[0].number > pair->stack_a[1].number)
-	// 		instructions->arr[instructions->index++] = stack_rotate_a(*pair);
-	// 	else
-	// 		instructions->arr[instructions->index++] = stack_rotate_rev_a(*pair);
-	// }
 }
 
-static t_stack	*get_cheapest_b(t_stack_pair *pair)
+// while (!is_stack_sorted(pair->stack_a, pair->a_length))
+// {
+// 	if (pair->stack_a[0].number > pair->stack_a[1].number
+//&& pair->stack_a[0].number > pair->stack_a[2].number)
+// 		instructions->arr[instructions->index++] = stack_swap_a(*pair);
+// 	else if (pair->stack_a[0].number > pair->stack_a[1].number)
+// 		instructions->arr[instructions->index++] = stack_rotate_a(*pair);
+// 	else
+// 		instructions->arr[instructions->index++] = stack_rotate_rev_a(*pair);
+// }
+
+// static int	calculate_cost(t_stack_pair *pair, int i, int *cheapest_index)
+// {
+// 	int	cost;
+// 	int	j;
+
+// 	*cheapest_index = get_place_before(pair->stack_a,
+// 			pair->a_length, pair->stack_b[i].number).index;
+// 	if (*cheapest_index < 0)
+// 		return (*cheapest_index);
+// 	j = i;
+// 	if (i > pair->b_length / 2)
+// 		j = pair->b_length - i;
+// 	cost = *cheapest_index + j;
+// 	if (cost - j > pair->a_length / 2)
+// 		cost = pair->a_length - cost;
+// 	return (cost);
+//}
+
+static void	get_cheapest_b(t_stack_pair *pair, t_stack arr[2])
 {
 	int		current_cost;
 	int		tmp_cost;
 	int		tmp_index;
-	t_stack	*target;
 	int		i;
 	int		j;
 
 	current_cost = INT_MAX;
-	target = malloc(sizeof(t_stack) * 2);
-	if (!target)
-		return (NULL);
-	target[0].index = -1;
-	target[1].index = -1;
+	arr[0].index = -1;
+	arr[1].index = -1;
 	i = 0;
 	while (i < pair->b_length)
 	{
 		tmp_index = get_place_before(pair->stack_a,
 				pair->a_length, pair->stack_b[i].number).index;
-		if (tmp_index == -1)
-			break ;
+		if (tmp_index < 0)
+			return ;
 		j = i;
 		if (i > pair->b_length / 2)
 			j = pair->b_length - i;
@@ -79,25 +96,22 @@ static t_stack	*get_cheapest_b(t_stack_pair *pair)
 		if (tmp_cost < current_cost)
 		{
 			current_cost = tmp_cost;
-			target[0] = pair->stack_a[tmp_index];
-			target[1] = pair->stack_b[i];
+			arr[0] = pair->stack_a[tmp_index];
+			arr[1] = pair->stack_b[i];
 		}
 		i++;
 	}
-	//printf("Cheapest a (n / i): %i %i, b (n / i): %i %i\n", target[0].number, target[0].index, target[1].number, target[1].index);
-	return (target);
 }
 
 static void	sort_stack(t_stack_pair *pair, t_instructions *instructions)
 {
-	t_stack	*cheapest;
+	t_stack	cheapest[2];
 
 	while (pair->b_length)
 	{
-		cheapest = get_cheapest_b(pair);
+		get_cheapest_b(pair, cheapest);
 		if (cheapest->index > -1)
 			stack_ab_move_to_first(*pair, cheapest, instructions);
-		free(cheapest);
 		instructions->arr[instructions->index++] = stack_push_b_to_a(pair);
 		if (pair->stack_a[0].number < pair->stack_a[1].number
 			&& pair->stack_a[0].number
