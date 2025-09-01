90910f38bc31f75d314cb1e82851ca4eb090c2f5
YeDemirkiran <yedemirkiran27@gmail.com>
2025-09-01 22:43:43 +0300

    Add todo and test_threads()

--- philosophers/philo/src/main.c
+++ philosophers/philo/src/main.c
@@ -6,17 +6,33 @@
 /*   By: yademirk <yademirk@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 21:35:53 by yademirk          #+#    #+#             */
-/*   Updated: 2025/09/01 21:47:29 by yademirk         ###   ########.fr       */
+/*   Updated: 2025/09/01 22:43:05 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#define _DEFAULT_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void	test_threads(void)
+{
+	int			i;
+	useconds_t	t;
+
+	i = -1;
+	t = 1000000;
+	while (i < 100)
+	{
+		printf("Thread %i accessed i: now %i\n", 0, ++i);
+		usleep(t);
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	test_threads();
 	return (EXIT_SUCCESS);
 }
